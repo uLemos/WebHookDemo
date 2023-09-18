492d7bc0c56510f8adfacb0adeeb2ffb8f989dc1
uLemos <fsnvag12@gmail.com>
2023-09-18 20:31:20 -0300

    :memo: Testando WebHook GitHub

---
+++ WebHookGitHub/Controllers/WebHookController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace WebHookGitHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebHookController : ControllerBase
+    {
+        [GitHubWebHook]
+        public IActionResult GitHubHandler(JObject data)
+        {
+            var result = JsonConvert.SerializeObject(data);
+            return Ok();
+        }
+    }
+}
